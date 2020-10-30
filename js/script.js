f5a7a90efc95d3b81a4ebf56791aa60058221425
alex Jaspers <xpsion@gmail.com>
2020-10-30 04:00:15 +0100

    correction
    ' instead of "

--- js/script.js
+++ js/script.js
@@ -80,6 +80,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // *** start ***
 // On first load, show home view
+
+// alex: value by buildAndShowHomeHTML
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,buildAndShowHomeHTML,true); 
@@ -101,6 +103,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
+			// Alex: by short_name 
 var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -115,16 +118,14 @@ var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
-			
-			
-			
+			//Alex:
+var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName","'"+chosenCategoryShortName+"'");
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-
+//Alex:push to main page
 			insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 			
     },
